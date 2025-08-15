eaca97806ed2565b65217979b9e488e37ac44ffa
Alam Dominic <alamdominic16@gmail.com>
2025-08-14 18:16:14 -0600

    Subo cambios en Purchase: Model, controller, repository, exception, service

--- src/main/java/com/repairman/repairman/service/PurchaseService.java
+++ src/main/java/com/repairman/repairman/service/PurchaseService.java
@@ -1,7 +1,11 @@
 package com.repairman.repairman.service;
 
+import com.repairman.repairman.exceptions.CustomerNotFoundException;
 import com.repairman.repairman.exceptions.PurchaseCustomerNotFoundException;
+import com.repairman.repairman.exceptions.PurchaseNotFoundException;
+import com.repairman.repairman.model.CustomerModel;
 import com.repairman.repairman.model.PurchaseModel;
+import com.repairman.repairman.repository.CustomerRepository;
 import com.repairman.repairman.repository.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,62 +14,76 @@ import java.util.List;
 
 @Service
 public class PurchaseService {
-
     @Autowired
     private PurchaseRepository purchaseRepository;
+    // Se inyecta para hacer validaciones
+    private final CustomerRepository customerRepository;
 
-    public PurchaseService(PurchaseRepository purchaseRepository) {
+    //constructor de la clase CustomerService y su función es recibir una dependencia (CustomerRepository)
+    // y asignarla al campo interno de la clase para poder usarla.
+    public PurchaseService(PurchaseRepository purchaseRepository, CustomerRepository customerRepository) {
         this.purchaseRepository = purchaseRepository;
+        this.customerRepository = customerRepository;
     }
 
-    // Método para devolver TODAS las compras
-    public List<PurchaseModel> getAllPurchases(){
+    // Método para devolver todos los purchases
+    public List<PurchaseModel> getPurchases(){
         return purchaseRepository.findAll();
     }
 
-    // Método para buscar compras por ID de customer
-    public List<PurchaseModel> findByCustomerID(Long customerID){
-        List<PurchaseModel> purchases = purchaseRepository.findByCustomerId(customerID);
-
-        // Si no encuentra compras para ese customer, lanza excepción
-        if (purchases.isEmpty()) {
-            throw new PurchaseCustomerNotFoundException(customerID);
-        }
-
-        return purchases;
+    // Método para devolver un único customer una Exception
+    public PurchaseModel findById (Long id){
+        // lanzamos una exception usando lambda orElseThrow
+        return purchaseRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException(id));
     }
 
-    // Método para buscar UNA compra por su propio ID
-    public PurchaseModel findByPurchaseID(Long purchaseID){
-        return purchaseRepository.findByPurchaseID(purchaseID)
-                .orElseThrow(() -> new RuntimeException("No se encontró la compra con ID: " + purchaseID));
+    // Método para crear nuevo Compra
+    public CustomerModel createCustomer(CustomerModel newCustomer){
+        return customerRepository.save(newCustomer);
     }
 
     // Método para crear una nueva compra
-    public PurchaseModel createPurchase(PurchaseModel purchase) {
-        return purchaseRepository.save(purchase);
+    public PurchaseModel addPurchase(PurchaseModel newPurchase) {
+        CustomerModel customerPurchase = newPurchase.getCustomer();
+
+        // Busca al Customer en la base de datos usando su ID
+        CustomerModel existingCustomer = customerRepository.findById(customerPurchase.getCustomerID())
+                .orElseThrow(() -> new CustomerNotFoundException(customerPurchase.getCustomerID())); // <-- Maneja el caso si el cliente no existe
+
+        newPurchase.setCustomer(existingCustomer); // Asocia el customerID con la una nueva Sale
+        return purchaseRepository.save(newPurchase);
     }
 
-    // Método para actualizar una compra
-    public PurchaseModel updatePurchase(Long purchaseID, PurchaseModel updatedPurchase) {
-        return purchaseRepository.findByPurchaseID(purchaseID).map(purchase -> {
-            purchase.setBrand(updatedPurchase.getBrand());
-            purchase.setPhoneStatus(updatedPurchase.getPhoneStatus());
-            purchase.setDescription(updatedPurchase.getDescription());
-            purchase.setPrice(updatedPurchase.getPrice());
-            purchase.setCreatedAt(updatedPurchase.getCreatedAt());
-            purchase.setCustomer(updatedPurchase.getCustomer());
-
-            return purchaseRepository.save(purchase);
-        }).orElseThrow(() -> new RuntimeException("No se encontró la compra con ID: " + purchaseID));
+    // Recuperar una venta por su Id
+    public PurchaseModel getById(Long id) {
+        return purchaseRepository.findById(id)
+                .orElseThrow( () -> new PurchaseCustomerNotFoundException(id));
     }
 
-    // Método para eliminar una compra
-    public void deletePurchase(Long purchaseID) {
-        if (purchaseRepository.existsById(purchaseID)) {
-            purchaseRepository.deleteById(purchaseID);
+    // Eliminar una venta por id deleteById()
+    public void deleteById(Long id) {
+        if(purchaseRepository.existsById(id)) {
+            purchaseRepository.deleteById(id);
         } else {
-            throw new RuntimeException("No se encontró la compra con ID: " + purchaseID);
+            throw new PurchaseNotFoundException(id);
         }
+    }
+
+    // Actualizar información de venta
+    public PurchaseModel updateSale(PurchaseModel purchase, Long id) {
+        PurchaseModel onePurchase = purchaseRepository.findById(id)
+                .orElseThrow(() -> new PurchaseCustomerNotFoundException(id));
+
+        onePurchase.setBrand(purchase.getBrand());
+        onePurchase.setPhoneModel(purchase.getPhoneModel());
+        onePurchase.setPhoneStatus(purchase.getPhoneStatus());
+        onePurchase.setDescription(purchase.getDescription());
+        onePurchase.setPrice(purchase.getPrice());
+        onePurchase.setImageUrl(purchase.getImageUrl());
+
+
+        return purchaseRepository.save(onePurchase);
+
+
     }
 }
